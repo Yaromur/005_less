211c008f6d4fe61221d8b3c184a96bfdfa9acd5f
h <h@h.h>
2022-12-07 18:56:02 +0200

    Add project files.

---
+++ 005_less/Source.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<string>
+#include<numeric> 
+#include<algorithm> 
+
+using namespace std;
+
+class car {
+	string name;
+	int age;
+	int	ob;
+	int price;
+public:
+	car(string name, int age, int ob, int price) {
+		this->name = name;
+		this->age = age;
+		this->ob = ob;
+		this->price = price;
+	}
+
+	string
+};
+
+int main() {
+
+
+
+
+	return 0;
+}
